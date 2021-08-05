3b02be8b1a337bccfb1ecf86362107e574d9c944
Jason Zhong <51380978+Jzhong456@users.noreply.github.com>
2021-08-05 16:16:25 -0400

    added clerk provider (#6)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,30 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { useRouter } from 'next/dist/client/router';
+import React from 'react';
+import {
+  ClerkProvider,
+  RedirectToSignIn,
+  SignedIn,
+  SignedOut
+} from "@clerk/clerk-react";
+const clerkFrontendApi = process.env.NEXT_PUBLIC_CLERK_FRONTEND_API;
+const clerkSignInURL = process.env.NEXT_PUBLIC_CLERK_SIGN_IN;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  return <ClerkProvider
+    frontendApi={clerkFrontendApi}
+    navigate={(to) => router.push(to)}
+>
+  <>
+    <SignedIn>
+      <Component {...pageProps} />
+    </SignedIn>
+    <SignedOut>
+      <RedirectToSignIn />
+    </SignedOut>
+  </>
+</ClerkProvider>
 }
-export default MyApp
+export default MyApp;
