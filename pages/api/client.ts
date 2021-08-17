45e92248033f944ca7de20277545314f8ab745f0
Grace Wang <gracewang92@gmail.com>
2021-08-16 17:07:51 -0700

    format PG dates as YYYY-MM-DD in API

--- pages/api/client.ts
+++ pages/api/client.ts
@@ -1,7 +1,9 @@
 
+import moment from 'moment';
 import { Client, types } from 'pg';
 
 types.setTypeParser(types.builtins.NUMERIC, (val: string) => Number(val));
+types.setTypeParser(types.builtins.DATE, (val: string) => moment(val).format("YYYY-MM-DD"));
 const client = new Client();
 if (process.env.NODE_ENV !== "test") {
   client.connect();
