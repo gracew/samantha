e8b3b3f51bac64e480174a8826b07213a1b5a150
Jason <jrzhong2009@gmail.com>
2021-08-20 22:17:05 -0400

    filled checkin with skeleton

---
+++ pages/checkins.tsx
@@ -0,0 +1,12 @@
+import styles from '../styles/Home.module.css';
+import { Button, Card, CardBody, CardHeader } from 'grommet';
+import { useRouter } from 'next/dist/client/router';
+import React, { useEffect, useState } from 'react';
+
+export default function CheckIns(){
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
