916e67206bcfc3125100239169cb26d30d474879
Grace Wang <gracewang92@gmail.com>
2021-08-25 10:48:28 -0700

    override calendar child prop

--- pages/api/getCheckins.ts
+++ pages/api/getCheckins.ts
@@ -6,11 +6,9 @@ async function handler(
     req: RequireSessionProp<NextApiRequest>,
     res: NextApiResponse
   ) {
-      const query = `SELECT id, emotion, notes
-      FROM checkins
-      WHERE user_id = $1`
+      const query = `SELECT * FROM checkins WHERE user_id = $1`
 
-      const pgRes = await client.query(query, [req.session.userId, req.body.id]);
+      const pgRes = await client.query(query, [req.session.userId]);
       res.status(200).json(pgRes.rows);
   }
   
--- pages/checkins.tsx
+++ pages/checkins.tsx
@@ -1,32 +1,56 @@
 import styles from '../styles/Home.module.css';
-import { Calendar, Box } from 'grommet';
+import { Calendar, Box, Button } from 'grommet';
 import { useRouter } from 'next/dist/client/router';
 import PrevButton from '../components/prevButton';
 import React, { useEffect, useState } from 'react';
 import { getCheckins, Person } from '../store';
+import moment from 'moment';
 
-export default function CheckIns(){
-    const router = useRouter();
-    const [checkins, setCheckins] = useState<any>();
+export default function CheckIns() {
+  const router = useRouter();
+  const [checkins, setCheckins] = useState<Record<string, any>>({});
 
-    useEffect(() => {
-        getCheckins((result:any)=> {
-            setCheckins(result)
-        });
-      }, []);
-    return (
-        <div className={styles.container}>
-            <main className={styles.main}>
-            <PrevButton href="/checkins/new"/>
-            <Box pad="large" align="center">
-                <Calendar
-                    size="medium"
-                    date={(new Date()).toISOString()}
-                    onSelect={(date) => {}}
-                    onClick={() => router.push(`/`)}
-                />
-            </Box>
-            </main>
-        </div>
-    )
+  useEffect(() => {
+    getCheckins().then((result: any[]) => {
+      // result is of type list
+      const resultMap: Record<string, any> = {};
+      result.forEach(c => {
+        resultMap[moment(c.created_at).format("YYYY-MM-DD")] = c;
+      })
+      setCheckins(resultMap);
+    });
+  }, []);
+
+  function renderDate({ date }: { date: Date }) {
+    const formattedDate = moment(date).format("YYYY-MM-DD");
+    const checkin = checkins[formattedDate];
+    return <div>
+      {checkin && checkin.emotion}
+      {date.getDate()}
+    </div>;
+  }
+
+  function onSelect(date: string | string[]) {
+    const formattedDate = moment(date).format("YYYY-MM-DD");
+    const checkin = checkins[formattedDate];
+    if (checkin) {
+      router.push(`/checkins/${checkin.id}`)
+    }
+  }
+
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <PrevButton href="/checkins/new" />
+        <Box pad="large" align="center">
+          <Calendar
+            size="medium"
+            date={(new Date()).toISOString()}
+            onSelect={onSelect}
+            children={renderDate}
+          />
+        </Box>
+      </main>
+    </div>
+  )
 }
