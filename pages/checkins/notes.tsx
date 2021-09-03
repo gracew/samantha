9f1b298b19e46de9e0bf80a5272a1d5b8dfde274
Jason <jrzhong2009@gmail.com>
2021-09-03 16:11:44 -0400

    minor changes

--- pages/checkins/notes.tsx
+++ pages/checkins/notes.tsx
@@ -5,6 +5,37 @@ import { emotionDict } from '.';
 import { Step } from '../../components/step';
 import styles from '../../styles/Form.module.css';
 
+const affirmation: Record<string, Array<string>> = {
+  Happy: ["Ride that high sunshine!",
+  "When you're in your flow, you'll continue to glow",
+  "You're in that flow and you've got that glow"],
+  Excited: ["It's easy to thrive when we feel alive",
+  "Love springs from the inside"],
+  Inspired: ["The world's your oyster!",
+  "Anything is possible!"],
+  Unsure: ["It's okay not to know.",
+  "Slow and steady wins the race.",
+  "Introspection can provide answers.",
+  "Take it one day at a time."],
+  Bored: ["Boredom is a pattern, not a reality.",
+  "The cure for boredom is curiosity",
+  "Boredom hits everyone from time to time.",
+  '"Isn\'t history ultimately the result of our fear of boredom?" - Emily M. Cioran'],
+  Anxious: ["Trust yourself. You've survived a lot, and you'll survive whatever is coming.",
+  "Just when the caterpillar thought the world was ending, he turned into a butterfly.", "You cannot always control what goes on outside, but you can always control what goes on inside.", "Feelings come and go like clouds in a windy sky."],
+  Angry: ["Not every bridge needs a fire.",
+  "This too shall pass",
+  "We boil at different degrees",
+  "Anger can bring about momentum for change"],
+  Stress: ["Don't worry",
+  "Calmness is the cradle of power.",
+  "Breathe in, breathe out."],
+  Sad:["The road can be rough, but it gets smoother.",
+  "This too shall pass",
+  "There's light at the end of the tunnel",
+  "Even the darkest night will end and the sun will rise."]
+}
+
 export default function Notes() {
   const [text, setText] = useState<string>();
   const [loading, setLoading] = useState(false);
@@ -23,37 +54,6 @@ export default function Notes() {
   }
 
   function getAffirmation(emotion:string){
-    const affirmation: Record<string, Array<string>> = {
-      Happy: ["Ride that high sunshine!",
-      "When you're in your flow, you'll continue to glow",
-      "You're in that flow and you've got that glow"],
-      Excited: ["It's easy to thrive when we feel alive",
-      "Love springs from the inside"],
-      Inspired: ["The world's your oyster!",
-      "Anything is possible!"],
-      Unsure: ["It's okay not to know.",
-      "Slow and steady wins the race.",
-      "Introspection can provide answers.",
-      "Take it one day at a time."],
-      Bored: ["Boredom is a pattern, not a reality.",
-      "The cure for boredom is curiosity",
-      "Boredom hits everyone from time to time.",
-      '"Isn\'t history ultimately the result of our fear of boredom?"- Emily M. Cioran'],
-      Anxious: ["Trust yourself. You've survived a lot, and you'll survive whatever is coming.",
-      "Just when the caterpillar thought the world was ending, he turned into a butterfly.", "You cannot always control what goes on outside, but you can always control what goes on inside.", "Feelings come and go like clouds in a windy sky."],
-      Angry: ["Not every bridge needs a fire.",
-      "This too shall pass",
-      "We boil at different degrees",
-      "Anger can bring about momentum for change"],
-      Stress: ["Don't worry",
-      "Calmness is the cradle of power.",
-      "Breathe in, breathe out."],
-      Sad:["The road can be rough, but it gets smoother.",
-      "This too shall pass",
-      "There's light at the end of the tunnel",
-      "Even the darkest night will end and the sun will rise."]
-    }
-
     let random = 0
 
     switch(emotion){
