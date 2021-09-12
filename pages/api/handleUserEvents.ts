640020990eafd487502f104cc4791887a0d9ee71
Grace Wang <gracewang92@gmail.com>
2021-09-12 00:11:33 -0700

    changes to support phone number auth

--- pages/api/handleUserEvents.ts
+++ pages/api/handleUserEvents.ts
@@ -25,13 +25,25 @@ export default async function handler(
   }
 
   if (msg.type === "user.created") {
-    const query = "insert into users(id, first_name, last_name, email) values ($1, $2, $3, $4)";
-    await client.query(query, [
-      msg.data.id,
-      msg.data.first_name,
-      msg.data.last_name,
-      msg.data.email_addresses[0].email_address
-    ]);
+    if (msg.data.phone_numbers.length > 0) {
+      await client.query(
+        "insert into users(id, first_name, last_name, phone) values ($1, $2, $3, $4)",
+        [
+          msg.data.id,
+          msg.data.first_name,
+          msg.data.last_name,
+          msg.data.phone_numbers[0].phone_number,
+        ]);
+    } else if (msg.data.email_addresses.length > 0) {
+      await client.query(
+        "insert into users(id, first_name, last_name, email) values ($1, $2, $3, $4)",
+        [
+          msg.data.id,
+          msg.data.first_name,
+          msg.data.last_name,
+          msg.data.email_addresses[0].email_address
+        ]);
+    }
   }
   res.status(200).end();
 }
---
+++ scripts/2021-09-11.sql
@@ -0,0 +1,3 @@
+alter table users
+alter column email drop not null,
+add column phone text;
